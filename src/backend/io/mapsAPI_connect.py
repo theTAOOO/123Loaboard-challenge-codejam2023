0c02750f9d8b160bb31e92365aba4d69ff5b1d83
lucas-qian <122037829+lucas-qian@users.noreply.github.com>
2023-11-18 17:16:35 -0500

    Calculating the distance between trucker position and package location origin works

--- src/backend/io/mapsAPI_connect.py
+++ src/backend/io/mapsAPI_connect.py
@@ -1,10 +1,9 @@
-import requests
 import json
+import googlemaps
 
-def ConnectAPI():
-    with open('src/backend/io/maps_connect.json', 'r') as json_file:
-        data = json.load(json_file)
-    api_key = data["apiKey"]
+def connectAPI():
+    with open('maps_connect.json', 'r') as json_file:
+        api_key = json.load(json_file)
 
     with open('file1.json', 'r') as startpoint:
         data1 = json.load(startpoint)
@@ -12,24 +11,25 @@ def ConnectAPI():
     with open('file2.json', 'r') as endpoint:
         data2 = json.load(endpoint)
 
+    maps_client = googlemaps.Client(key=api_key["apiKey"])
     start_lat = data1["positionLatitude"]
     start_lng = data1["positionLongitude"]
     end_lat = data2["originLatitude"]
     end_lng = data2["originLongitude"]
-    origin = 'start_lat,start_lng'
-    destination = 'end_lat,end_lng'
-    url = f'https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={destination}&key={api_key}'
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-        distance_text = data['rows'][0]['elements'][0]['distance']['text']
-        distance_value = data['rows'][0]['elements'][0]['distance']['value']
+    origin = (start_lat, start_lng)
+    destination = (end_lat, end_lng)
+
+    # Using googlemaps library to get directions
+    directions = maps_client.directions(origin, destination, mode="driving")
+
+    if directions:
+        distance_text = directions[0]['legs'][0]['distance']['text']
+        distance_value = directions[0]['legs'][0]['distance']['value']
 
         print(f'Distance: {distance_text}')
         print(f'Distance Value: {distance_value} meters')
     else:
-        print(f'Error: {response.status_code}')
-        print(response.text)
+        print('Error calculating distance using googlemaps library')
 
-connectedAPI = ConnectAPI()
+connectAPI()
