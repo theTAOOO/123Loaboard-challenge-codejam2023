e2a8f35d82062140ca6e5079a8be6f526b5966b9
lucas-qian <122037829+lucas-qian@users.noreply.github.com>
2023-11-18 18:22:19 -0500

    Definition returns a tuple of 2 elements: (Distance, Estimated Travel Time)

--- src/backend/io/mapsAPI_connect.py
+++ src/backend/io/mapsAPI_connect.py
@@ -2,7 +2,7 @@ import json
 import googlemaps
 
 
-def connectAPI():
+def connectAPI() -> (str, str):
     with open('maps_connect.json', 'r') as json_file:
         api_key = json.load(json_file)
 
@@ -26,13 +26,16 @@ def connectAPI():
         distance_text = directions[0]['legs'][0]['distance']['text']
         distance_value = directions[0]['legs'][0]['distance']['value']
         duration_min = ((distance_value / 1609.34) / 65) * 60
-        duration_hour = str(int(duration_min/60))
+        duration_hour = str(int(duration_min / 60))
         duration_min_r = str(int(duration_min % 60))
         duration = duration_hour + ' hours ' + duration_min_r + ' min'
         print(f'Distance: {distance_text}')
         print(f'Estimated Travel Time: {duration}')
+        return f'Distance: {distance_text}', f'Estimated Travel Time: {duration}'
+
     except googlemaps.exceptions.ApiError as e:
         print(f'Error calculating distance using googlemaps library: {e}')
 
 
 connectAPI()
+
