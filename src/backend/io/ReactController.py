1efbfa619be9469db74ae749146e33b79e9ea2f3
AbsoluteBeginner <97850394+AbsoluteBeginner-0@users.noreply.github.com>
2023-11-19 02:00:02 -0500

    Implement WebSockets (#16)
    * Refactor to use WebSocket
    
    Please work ong
    
    * Configure WebSocket in backend

--- src/backend/io/ReactController.py
+++ src/backend/io/ReactController.py
@@ -10,19 +10,33 @@ class ReactController:
     def __init__(self, global_controller):
         self.GlobalController = global_controller
         self.app = Flask(__name__)
-        CORS(self.app)
-        self.socketio = SocketIO(self.app)
+        # CORS(self.app)
+        self.socketio = SocketIO(self.app, cors_allowed_origins="*", logger=True, engineio_logger=True)
         self.setup_routes()
 
     def start_thread(self):
         self.app.run(debug=False)
+        self.socketio.emit('message_from_server', 'Hello from the server!')
 
     def setup_routes(self):
+        self.app.route('/', methods=['GET'])(self.get_data)
+        self.app.route('/get_data', methods=['GET'])(self.get_data)
         self.app.route('/api/data', methods=['GET'])(self.get_data)
+        self.socketio.on('message_from_client')(self.handle_message)
+        self.socketio.on('connect')(self.say_hello)
 
-    def get_data(something):
-        print(something)
+    def get_data(message):
+        print(message)
         data = {'message': 'Hello from Flask!'}
         response = jsonify(data)
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
+
+    def handle_message(self, message):
+        print('Received message:', message)
+        self.socketio.emit('message_from_server', message)
+
+    def say_hello(self):
+        print('Client connected')
+        self.socketio.emit('welcome_message', 'Welcome to the Socket.IO server!') 
+    
---
+++ src/frontend/src/SocketComponent.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
+
+const SocketComponent = () => {
+  useEffect(() => {
+    const socket = io('http://localhost:5000', {transports: ['websocket']});
+
+    socket.on('connect', () => {
+      console.log('Connected to the server');
+    });
+
+    socket.on('message_from_server', (data) => {
+      console.log('Received message from server:', data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* Render your component content here */}
+    </div>
+  );
+};
+
+export default SocketComponent;
