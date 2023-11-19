34ea0d053bc7e175518b7d8728fb8dab497e6e13
theTAOOO <tao.darren@gmail.com>
2023-11-18 22:27:58 -0500

    still have border and marker issues

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,9 @@
 // App.js
+import './index.css';
 import React from 'react';
 import Map from './map';
+import swift from "./swifttrack.png";
+
 
 const coordinates = [
   { lat: 37.7749, lng: -122.4194 }, // San Francisco
@@ -10,10 +13,53 @@ const coordinates = [
 
 const App = () => {
   return (
-    <div>
-      <Map coordinates={coordinates} title="Welcome back, Yumeng." />
-    </div>
+    <><div className="divmap">
+      <Map coordinates={coordinates}/>
+    </div><div className="divmap"
+      style={{
+        position: 'absolute',
+        top: '5%',
+        left: '4%',
+        zIndex: '1',
+        textAlign: 'left',
+        color: 'white',
+        fontFamily: 'Helvetica',
+        fontSize: '11vmin',
+        fontWeight: 'bold',
+      }}
+    >
+        Welcome back, Yumeng.
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          top: '19%',
+          left: '7%',
+          zIndex: '1',
+          textAlign: 'left',
+          color: 'white',
+          fontFamily: 'Helvetica',
+          fontSize: '5vmin',
+          fontWeight: 'bold',
+        }}
+      >
+        We have a package for you to collect.
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          top: '89%',
+          left: '77%',
+          zIndex: '5',
+          textAlign: 'left',
+        }}>
+        <img src={swift} alt= "logo" style={{ width: '300px', height: 'auto' }}/>
+      </div>
+      </>
+    
   );
+  
 };
 
 export default App;
+
