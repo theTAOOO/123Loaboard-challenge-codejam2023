131174578eb156cd321750cb4d2726e0bbd1e86d
jdawg <jackcxcai@gmail.com>
2023-11-18 22:02:55 -0500

    Start work on ReactController

---
+++ src/backend/io/ReactController.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+from flask_socketio import SocketIO
+
+class ReactController:
+    app = None 
+    GlobalController = None
+    socketio = None
+
+    def __init__(self, global_controller):
+        self.GlobalController = global_controller
+        self.app = Flask(__name__)
+        CORS(self.app)
+        self.socketio = SocketIO(self.app)
+        self.setup_routes()
+        self.app.run(debug=True)
+
+    def setup_routes(self):
+        self.app.route('/api/data', methods=['GET'])(self.get_data)
+
+    def get_data(something):
+        print(something)
+        data = {'message': 'Hello from Flask!'}
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
