9a3b3dff01fc96dbafd9972492b24d9397d5899f
jdawg <jackcxcai@gmail.com>
2023-11-18 22:03:31 -0500

    Init connection with Flask server on frontend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,6 @@
 // App.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Map from './map';
 
 const coordinates = [
@@ -9,6 +10,18 @@ const coordinates = [
 ];
 
 const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data')
+        .then(response => {
+            setData(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}, []);
+
   return (
     <div>
       <Map coordinates={coordinates} title="Welcome back, Yumeng." />
